ec3ee0ce0149a365e30b369e3c749a7a11e583cb
Ulmeken Kozybaeva <136685941+ulmekenkozybaeva@users.noreply.github.com>
2024-05-21 16:29:59 +0500

    Update models.py

--- todo_list/todoapp/models.py
+++ todo_list/todoapp/models.py
@@ -6,15 +6,6 @@ from django.urls import reverse
 def one_week_hence():
     return timezone.now() + timezone.timedelta(days=7)
 
-# class ToDoStatus(models.Model):
-#     title = models.CharField(max_length=100, unique=True)
-#
-#     def get_absolute_url(self):
-#         return reverse("list", args=[self.id])
-#
-#     def __str__(self):
-#         return self.title
-
 class ToDoList(models.Model):
     title = models.CharField(max_length=100, unique=True)
 
